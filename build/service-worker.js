1d6436539cf5751788a703549934ad2f5d2744db
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-14 14:55:51 +0100

    Change name

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2e66af8477f0ccfa10d3deb6cb94d3a.js"
+  "/Books-dnd/precache-manifest.6614f90cd178af25017a67a039b5fb01.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Books-dnd/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
